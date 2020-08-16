f7d38730feeeb292a44f20b8cfeb7896dbc83384
sho shimohata <pontatanpo1014@gmail.com>
2020-08-16 23:39:04 +0900

    エッジと接続

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -2,6 +2,14 @@ const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs = `
 
+  type User{
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+  }
+
   enum PhotoCategory{
     SELFIL
     PORTRAIT
@@ -16,6 +24,8 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -34,7 +44,42 @@ const typeDefs = `
   }
 `
 
-var photos = []
+var users = [
+  { "githubLogin": "mHattrup", "name": "Mike Hattrup"},
+  { "githubLogin": "gPlake", "name": "Glen Plake"},
+  { "githubLogin": "sSchmidt", "name": "Scot Schemidt"},
+]
+
+var photos = [
+  {
+    "id": "1",
+    "name": "Dropping",
+    "description": "The Heart",
+    "category": "ACTION",
+    "githubUser": "gPlake",
+  },
+  {
+    "id": "2",
+    "name": "Enjoy",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt",
+  },
+  {
+    "id": "3",
+    "name": "Gunbarrel",
+    "description": "25 laps",
+    "category": "LANDSCAPE",
+    "githubUser": "sSchmidt",
+  },
+]
+
+var tags =[
+  {"photoID": "1", "userID": "gPlake"},
+  {"photoID": "2", "userID": "sSchmidt"},
+  {"photoID": "2", "userID": "mHattrup"},
+  {"photoID": "2", "userID": "gPlake"},
+]
+
 var _id = 0
 
 const resolvers = {
@@ -54,7 +99,23 @@ const resolvers = {
     }
   },
   Photo:{
-    url: parent => `http://yoursite.som/img/${ parent.id }.jpg`
+    url: parent => `http://yoursite.som/img/${ parent.id }.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    },
+    taggedUsers: parent => tags
+    .filter(tag => tag.photoID === parent.id)
+    .map(tag => tag.userID)
+    .map(userID => users.find(u => u.githubLogin === userID))
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
