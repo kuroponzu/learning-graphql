07b858d4ebbb80b24be88ca73a4a5a9271e1d886
sho shimohata <pontatanpo1014@gmail.com>
2020-08-24 23:41:58 +0900

    githubログイン機能実装

--- photo-share-api/resolvers/Mutation.js
+++ photo-share-api/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const { authorizeWithGithub } = require('../lib')
+
 module.exports = {
   // argsにはnameとdescriptionが含まれている。
   async postPhoto(parent, args){
@@ -7,5 +9,36 @@ module.exports = {
     }
     photos.push(newPhoto)
     return newPhoto
+  },
+
+  async githubAuth(parent, { code }, { db }) {
+    let {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code
+    })
+
+    if (message){
+      throw new Error(message)
+    }
+
+    let latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url
+    }
+
+    const{ ops:[user] } = await db
+      .collection('users')
+      .replaceOne({ githubLogin: login}, latestUserInfo, { upsert: true })
+    
+    return { user, token: access_token }
   }
 }
