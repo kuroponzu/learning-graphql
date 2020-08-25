ea2db1e90590f745dc8d9d506ce51b7888033b57
sho shimohata <pontatanpo1014@gmail.com>
2020-08-26 00:49:57 +0900

    fakeUserの追加

--- photo-share-api/resolvers/Mutation.js
+++ photo-share-api/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 const { authorizeWithGithub } = require('../lib')
+const fetch = require('node-fetch')
 
 module.exports = {
   // argsにはnameとdescriptionが含まれている。
@@ -47,5 +48,33 @@ module.exports = {
       .replaceOne({ githubLogin: login}, latestUserInfo, { upsert: true })
     
     return { user, token: access_token }
+  },
+
+  addFakeUsers: async(root, {count}, {db}) => {
+    var randomUserApi = `https://randomuser.me/api/?results=${count}`
+
+    var { results } = await fetch(randomUserApi).then(res => res.json())
+
+    var users = results.map(r => ({
+      githubLogin: r.login.username,
+      name: `${r.name.first} ${r.name.last}`,
+      avator: r.picture.thumbnail,
+      githubToken: r.login.sha1
+    }))
+    console.log(users)
+    await db.collection('users').insertMany(users)
+
+    return users
+  },
+
+  async fakeUserAuth(parent, { githubLogin }, { db }){
+    var user = await db.collection(`users`).findOne({ githubLogin })
+    if(!user){
+      throw new Error(`Cannot find user with githubLogin ${githubLogin}`)
+    }
+    return {
+      token: user.githubToken,
+      user
+    }
   }
 }
